251743293d90d2f8458d74b7317987f92cd7e3ea
sujannani <sujan.kommalapati@gmail.com>
2024-09-05 10:45:11 +0530

    port

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask # type: ignore
 from flask_pymongo import PyMongo # type: ignore
 from flask_marshmallow import Marshmallow # type: ignore
 from config import Config
-
+import os
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -13,5 +13,5 @@ from controllers.user_controller import *
 from controllers.transaction_controller import *
 from controllers.category_controller import *
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
