823412ddab5c1e074b984af867c50820b4411fad
sujannani <sujan.kommalapati@gmail.com>
2024-12-20 11:53:23 +0530

    post

--- controllers/category_controller.py
+++ controllers/category_controller.py
@@ -8,7 +8,7 @@ obj=category_model()
 def add_category_controller():
     return obj.add_category_model(request.get_json())
 
-@app.route('/category/get_categories')
+@app.route('/category/get_categories',methods=["POST"])
 def get_categories_model():
     return obj.get_categories_model(request.get_json())
 
